0415c94de64aec1e825803d5ea6f8bd2f20e19ad
Kylee Bijerring <kylee24b@gmail.com>
2021-09-29 02:09:44 -0600

    routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    createReaction,
+    deleteReaction,
+    deleteThought,
+
+
+} = require('../../controllers/thought-controller')
+
+
+
+router
+    .route('/')
+    .get(getAllThoughts)
+
+
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought)
+
+
+
+router
+    .route('/:userId')
+    .post(createThought)
+
+
+
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction)
+
+
+
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction)
+
+
+
+module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUserById,
+    deleteUserById,
+    addFriend,
+    deleteFriend
+    
+} = require('../../controllers/user-controller')
+
+router
+
+.route('/')
+.get(getAllUsers)
+.post(createUser)
+
+router
+
+.route('/:id')
+.get(getUserById)
+.put(updateUserById)
+.delete(deleteUserById)
+
+router
+
+.route('/:id/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend)
+
+
+module.exports = router;
